6c5f6fa37d35f15bb76d2d6ec508730e32268aaa
Kendra <kendra@coffeeringsdesign.com>
2019-03-27 21:54:39 -0700

    begin to setup form for inputs

--- src/components/addNewSentenceForm.js
+++ src/components/addNewSentenceForm.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './../App.scss';
 
-class App extends Component {
+class addNewSentenceForm extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="addNewSentenceFormContent">
+        <form className="addNewSentenceForm" onSubmit={this.handleSubmit}>
+          <div>
+            <input type="checkbox" value="problemSolving" id="problemSolving" />
+            <label for="problemSolving">
+              Problem Solving
+            </label>
+          </div>
+          <div>
+            <input type="checkbox" value="crossTeamCollaboration" id="crossTeamCollaboration" />
+            <label for="crossTeamCollaboration">
+              Cross-Team Collaboration
+            </label>
+          </div>
+          <div>
+            <label for="enterParagraph">
+              Please enter the Paragraph:
+            </label>
+            <textarea value="enterParagraph" id="enterParagraph" />
+          </div>
+          <button className="button" type="submit">Add Sentence to Database</button>
+        </form>
       </div>
     );
   }
 }
 
-export default App;
+export default addNewSentenceForm;
